9fc80c3ec2dec5d38598b6213b54f10264a63147
GrapeApple <budouapplenoki@gmail.com>
2022-12-18 10:56:48 +0900

    Add favicon and License

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+/*
+* Copyright (c) 2022 Porlam Nicla
+* Licence 
+*/
 $url = ltrim(htmlspecialchars($_SERVER["REQUEST_URI"], ENT_QUOTES, 'UTF-8'), "/");
 $db = new SQLite3('url.db');
 $sql = <<<EOT
@@ -22,72 +26,75 @@ $res = $db->query($sql);
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	<title>OpenShorter</title>
 </head>
 
 <body>
 	<?php if ($url == "") { ?>
-		<div class="loading" id="loading-div">
-			<div class="loading-child">
-				<img src="/fox.gif" style="left: 50%;position: absolute;top: -50%;transform: translate(-50%, -50%);">
-				<p style="text-align: center; display: block;font-size: 34px;"><label style="color: #cc528b">Open</label><label style="color: #65ace4">Shorter</label></p>
+		<div style="height: 5px;background: linear-gradient(90deg,#cc528b 50%,transparent 0),linear-gradient(90deg,#65ace4 100%,transparent 0);">
+			<div class="loading" id="loading-div">
+				<div class="loading-child">
+					<img src="/fox.gif" style="left: 50%;position: absolute;top: -50%;transform: translate(-50%, -50%);">
+					<p style="text-align: center; display: block;font-size: 34px;"><label style="color: #cc528b">Open</label><label style="color: #65ace4">Shorter</label></p>
+				</div>
 			</div>
-		</div>
-		<?php
-		@ob_flush();
-		@flush();
-		sleep(2.5);
-		?>
-		<script>
-			var fadeOut = function(element, time, callback) {
-				var fadeTime = (time) ? time : 400,
-					keyFrame = 30,
-					stepTime = fadeTime / keyFrame,
-					minOpacity = 0,
-					stepOpacity = 1 / keyFrame,
-					opacityValue = 1;
+			<?php
+			@ob_flush();
+			@flush();
+			sleep(2.5);
+			?>
+			<script>
+				var fadeOut = function(element, time, callback) {
+					var fadeTime = (time) ? time : 400,
+						keyFrame = 30,
+						stepTime = fadeTime / keyFrame,
+						minOpacity = 0,
+						stepOpacity = 1 / keyFrame,
+						opacityValue = 1;
 
-				if (!element) return;
+					if (!element) return;
 
-				element.setAttribute('data-fade-stock-display', element.style.display.replace('none', ''));
+					element.setAttribute('data-fade-stock-display', element.style.display.replace('none', ''));
 
-				var setOpacity = function(setNumber) {
-					if ('opacity' in element.style) {
-						element.style.opacity = setNumber;
-					} else {
-						element.style.filter = 'alpha(opacity=' + (setNumber * 100) + ')';
+					var setOpacity = function(setNumber) {
+						if ('opacity' in element.style) {
+							element.style.opacity = setNumber;
+						} else {
+							element.style.filter = 'alpha(opacity=' + (setNumber * 100) + ')';
 
-						if (navigator.userAgent.toLowerCase().match(/msie/) &&
-							!window.opera && !element.currentStyle.hasLayout) {
-							element.style.zoom = 1;
+							if (navigator.userAgent.toLowerCase().match(/msie/) &&
+								!window.opera && !element.currentStyle.hasLayout) {
+								element.style.zoom = 1;
+							}
 						}
-					}
+					};
+
+					if (!callback || typeof callback !== 'function') callback = function() {};
+
+					setOpacity(1);
+
+					var sId = setInterval(function() {
+						opacityValue = Number((opacityValue - stepOpacity).toFixed(12));
+
+						if (opacityValue < minOpacity) {
+							opacityValue = minOpacity;
+							element.style.display = 'none';
+							clearInterval(sId);
+						}
+
+						setOpacity(opacityValue);
+
+						if (opacityValue === minOpacity) callback();
+					}, stepTime);
+
+					return element;
 				};
+				fadeOut(document.getElementById("loading-div"), 250);
+			</script>
+		</div>
 
-				if (!callback || typeof callback !== 'function') callback = function() {};
 
-				setOpacity(1);
-
-				var sId = setInterval(function() {
-					opacityValue = Number((opacityValue - stepOpacity).toFixed(12));
-
-					if (opacityValue < minOpacity) {
-						opacityValue = minOpacity;
-						element.style.display = 'none';
-						clearInterval(sId);
-					}
-
-					setOpacity(opacityValue);
-
-					if (opacityValue === minOpacity) callback();
-				}, stepTime);
-
-				return element;
-			};
-			fadeOut(document.getElementById("loading-div"), 250);
-		</script>
-		<?php
-		$db->close(); ?>
 	<?php } ?>
 </body>
 <?php
